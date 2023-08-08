6d7c905e3a5a29875db4583cb78dc9ef69d14b02
Julio Cesar Gonzaga <jaycgonzaga@gmail.com>
2023-08-08 22:54:47 +0800

    Optimized pokedex page and pokecard design, added styling for each pokemon type, remodeled navbar

--- src/components/poketype.jsx
+++ src/components/poketype.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import Pokemon from '../lib/pokemon'
+import { useState, React  } from 'react'
 
 export default function Poketype(type) {
+ 
+
   return (
     <div className='poketype flex justify-center mt-2'>
-        {type.type0 && <div className='typebox-grass mr-1 px-3'>{type.type0.charAt(0).toUpperCase() + type.type0.slice(1)}</div>}
-        {type.type1 && <div className='typebox-grass ml-1 px-3'>{type.type1.charAt(0).toUpperCase() + type.type1.slice(1)}</div>}
+        {type.type0 && <div className={`typebox-${type.type0} mr-1 px-3`}>{type.type0.charAt(0).toUpperCase() + type.type0.slice(1)}</div>}
+        {type.type1 && <div className={`typebox-${type.type0} ml-1 px-3`}>{type.type1.charAt(0).toUpperCase() + type.type1.slice(1)}</div>}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>   </React.StrictMode>
+  <React.StrictMode>   
     <App />
+  </React.StrictMode>
 );
 
 
